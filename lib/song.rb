734030e8942d74ec07659271f5ef99f9f987858c
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-03 05:39:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,11 +13,9 @@ class Song
   end
 
   def self.new_by_filename(file)
-  artist_name = file.split(" - ")[0]
-  song_name = file.split(" - ")[1]
-  song = Song.new(song_name)
-  song.artist = artist_name
-  song
-  end
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
 
 end
